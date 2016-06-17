0de48f4325a717858e6c3c846d4d5d0a1b1ca15f
Philip Hutchins <flipture@gmail.com>
2016-06-17 17:48:35 -0400

    blahhhh

--- init.sh
+++ init.sh
@@ -149,8 +149,8 @@ fi
 # Want to be able to only pull the users specified initial cookbook,
 # do the bundle install and berks install and go
 
-echo "Running Runner from $HOME_DIR/run.sh ..."
+echo "Running Runner from $BASE_DIR/run.sh ..."
 
 # Run chef-zero using env var for run_list
 env
-. $HOME_DIR/run.sh
+. $BASE_DIR/run.sh
